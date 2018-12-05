2389dab8425934f6a780f6cfb617b2a8e9efb8dc
Saptarshi Chatterjee <diesel@falcon.local>
2018-12-05 16:43:51 -0600

    works awesome

--- src/main/java/iit/talon/KafKaConsumer.java
+++ src/main/java/iit/talon/KafKaConsumer.java
@@ -1,6 +1,7 @@
 package iit.talon;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -38,16 +39,18 @@ public class KafKaConsumer {
     @KafkaListener(topics = "retrievedata", groupId = "coordinator")
     public void retrieveValue(String message) throws FileNotFoundException, IOException{
         String data[] = dataDeserializer(message);
-        BufferedInputStream reader1 = new BufferedInputStream(new FileInputStream(machine1+data[1]) );
-        BufferedInputStream reader2 = new BufferedInputStream(new FileInputStream(machine1+data[1]) );
-        BufferedInputStream reader3 = new BufferedInputStream(new FileInputStream(machine1+data[1]) );
+        BufferedInputStream reader1 = new BufferedInputStream(new FileInputStream(diskpath+"/"+data[1]) );
+        // BufferedInputStream reader2 = new BufferedInputStream(new FileInputStream(machine1+"/"+data[1]) );
+        // BufferedInputStream reader3 = new BufferedInputStream(new FileInputStream(machine2+"/"+data[1]) );
         boolean running = true;
         while( running ) {
-            if( reader1.available() > 0 && reader2.available() > 0 && reader3.available() > 0) {
+            //if( reader1.available() > 0 && reader2.available() > 0 && reader3.available() > 0) {
+            if( reader1.available() > 0 ) {
                 String val1 = IOUtils.toString(reader1, "UTF-8");
-                String val2 = IOUtils.toString(reader2, "UTF-8");
-                String val3 = IOUtils.toString(reader3, "UTF-8");
-                if(val1.equals(val2) && val2.equals(val3)){
+                // String val2 = IOUtils.toString(reader2, "UTF-8");
+                // String val3 = IOUtils.toString(reader3, "UTF-8");
+                //if(val1.equals(val2) && val2.equals(val3)){
+                if(val1.equals(val1)){
                     webSocket.convertAndSend("/topic/backToClient/"+data[0], val1);
                     running = false;
                 } else {
@@ -67,7 +70,7 @@ public class KafKaConsumer {
         logger.info(message);
     }
 
-    @KafkaListener(topics = "savedata")
+    @KafkaListener(topics = "savedata", groupId = "${diskpath.property}")
     public void saveDataToDisk(String message) throws IOException{
         String data[] = dataDeserializer(message);
         File file = new File(diskpath+"/"+data[0]);
